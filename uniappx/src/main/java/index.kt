4af72a9203be152bf4dfb981b5cc1197b06c8c0f
olatiao <a@rmb.sh>
2025-06-24 23:26:23 +0800

    feat: auto pack uniappx

--- uniappx/src/main/java/index.kt
+++ uniappx/src/main/java/index.kt
@@ -1,150 +1,124 @@
-@file:Suppress("UNCHECKED_CAST", "USELESS_CAST", "INAPPLICABLE_JVM_NAME", "UNUSED_ANONYMOUS_PARAMETER")
-package uni.UNI4CAF968;
-import io.dcloud.uniapp.*;
-import io.dcloud.uniapp.extapi.*;
-import io.dcloud.uniapp.framework.*;
-import io.dcloud.uniapp.runtime.*;
-import io.dcloud.uniapp.vue.*;
-import io.dcloud.uniapp.vue.shared.*;
-import io.dcloud.uts.*;
-import io.dcloud.uts.Map;
-import io.dcloud.uts.Set;
-import io.dcloud.uts.UTSAndroid;
-import kotlinx.coroutines.CoroutineScope;
-import kotlinx.coroutines.Deferred;
-import kotlinx.coroutines.Dispatchers;
-import kotlinx.coroutines.async;
-import io.dcloud.uniapp.extapi.exit as uni_exit;
-import io.dcloud.uniapp.extapi.showToast as uni_showToast;
+@file:Suppress("UNCHECKED_CAST", "USELESS_CAST", "INAPPLICABLE_JVM_NAME", "UNUSED_ANONYMOUS_PARAMETER", "NAME_SHADOWING", "UNNECESSARY_NOT_NULL_ASSERTION")
+package uni.UNIF95597E
+import io.dcloud.uniapp.*
+import io.dcloud.uniapp.extapi.*
+import io.dcloud.uniapp.framework.*
+import io.dcloud.uniapp.runtime.*
+import io.dcloud.uniapp.vue.*
+import io.dcloud.uniapp.vue.shared.*
+import io.dcloud.uts.*
+import io.dcloud.uts.Map
+import io.dcloud.uts.Set
+import io.dcloud.uts.UTSAndroid
+import io.dcloud.uniapp.extapi.exit as uni_exit
+import io.dcloud.uniapp.extapi.showToast as uni_showToast
 val runBlock1 = run {
     __uniConfig.getAppStyles = fun(): Map<String, Map<String, Map<String, Any>>> {
-        return GenApp.styles;
+        return GenApp.styles
     }
-    ;
 }
-var firstBackTime: Number = 0;
+var firstBackTime: Number = 0
 open class GenApp : BaseApp {
     constructor(__ins: ComponentInternalInstance) : super(__ins) {
         onLaunch(fun(_: OnLaunchOptions) {
-            console.log("App Launch");
+            console.log("App Launch")
         }
-        , __ins);
+        , __ins)
         onAppShow(fun(_: OnShowOptions) {
-            console.log("App Show");
+            console.log("App Show")
         }
-        , __ins);
+        , __ins)
         onAppHide(fun() {
-            console.log("App Hide");
+            console.log("App Hide")
         }
-        , __ins);
+        , __ins)
         onLastPageBackPress(fun() {
-            console.log("App LastPageBackPress");
+            console.log("App LastPageBackPress")
             if (firstBackTime == 0) {
-                uni_showToast(ShowToastOptions(title = "再按一次退出应用", position = "bottom"));
-                firstBackTime = Date.now();
+                uni_showToast(ShowToastOptions(title = "再按一次退出应用", position = "bottom"))
+                firstBackTime = Date.now()
                 setTimeout(fun(){
-                    firstBackTime = 0;
-                }, 2000);
+                    firstBackTime = 0
+                }, 2000)
             } else if (Date.now() - firstBackTime < 2000) {
-                firstBackTime = Date.now();
-                uni_exit(null);
+                firstBackTime = Date.now()
+                uni_exit(null)
             }
         }
-        , __ins);
+        , __ins)
         onExit(fun() {
-            console.log("App Exit");
+            console.log("App Exit")
         }
-        , __ins);
+        , __ins)
     }
     companion object {
-        val styles: Map<String, Map<String, Map<String, Any>>>
-            get() {
-                return normalizeCssStyles(utsArrayOf(
-                    styles0
-                ));
-            }
+        val styles: Map<String, Map<String, Map<String, Any>>> by lazy {
+            _nCS(_uA(
+                styles0
+            ))
+        }
         val styles0: Map<String, Map<String, Map<String, Any>>>
             get() {
-                return utsMapOf("uni-row" to padStyleMapOf(utsMapOf("flexDirection" to "row")), "uni-column" to padStyleMapOf(utsMapOf("flexDirection" to "column")));
+                return _uM("uni-row" to _pS(_uM("flexDirection" to "row")), "uni-column" to _pS(_uM("flexDirection" to "column")))
             }
     }
 }
 val GenAppClass = CreateVueAppComponent(GenApp::class.java, fun(): VueComponentOptions {
-    return VueComponentOptions(type = "app", name = "", inheritAttrs = true, inject = Map(), props = Map(), propsNeedCastKeys = utsArrayOf(), emits = Map(), components = Map(), styles = GenApp.styles);
+    return VueComponentOptions(type = "app", name = "", inheritAttrs = true, inject = Map(), props = Map(), propsNeedCastKeys = _uA(), emits = Map(), components = Map(), styles = GenApp.styles)
 }
 , fun(instance): GenApp {
-    return GenApp(instance);
+    return GenApp(instance)
 }
-);
+)
 val GenPagesIndexIndexClass = CreateVueComponent(GenPagesIndexIndex::class.java, fun(): VueComponentOptions {
-    return VueComponentOptions(type = "page", name = "", inheritAttrs = GenPagesIndexIndex.inheritAttrs, inject = GenPagesIndexIndex.inject, props = GenPagesIndexIndex.props, propsNeedCastKeys = GenPagesIndexIndex.propsNeedCastKeys, emits = GenPagesIndexIndex.emits, components = GenPagesIndexIndex.components, styles = GenPagesIndexIndex.styles, setup = fun(props: ComponentPublicInstance): Any? {
-        return GenPagesIndexIndex.setup(props as GenPagesIndexIndex);
-    }
-    );
+    return VueComponentOptions(type = "page", name = "", inheritAttrs = GenPagesIndexIndex.inheritAttrs, inject = GenPagesIndexIndex.inject, props = GenPagesIndexIndex.props, propsNeedCastKeys = GenPagesIndexIndex.propsNeedCastKeys, emits = GenPagesIndexIndex.emits, components = GenPagesIndexIndex.components, styles = GenPagesIndexIndex.styles)
 }
-, fun(instance): GenPagesIndexIndex {
-    return GenPagesIndexIndex(instance);
+, fun(instance, renderer): GenPagesIndexIndex {
+    return GenPagesIndexIndex(instance, renderer)
 }
-);
-val GenPagesIndex2Index2Class = CreateVueComponent(GenPagesIndex2Index2::class.java, fun(): VueComponentOptions {
-    return VueComponentOptions(type = "page", name = "", inheritAttrs = GenPagesIndex2Index2.inheritAttrs, inject = GenPagesIndex2Index2.inject, props = GenPagesIndex2Index2.props, propsNeedCastKeys = GenPagesIndex2Index2.propsNeedCastKeys, emits = GenPagesIndex2Index2.emits, components = GenPagesIndex2Index2.components, styles = GenPagesIndex2Index2.styles, setup = fun(props: ComponentPublicInstance): Any? {
-        return GenPagesIndex2Index2.setup(props as GenPagesIndex2Index2);
-    }
-    );
-}
-, fun(instance): GenPagesIndex2Index2 {
-    return GenPagesIndex2Index2(instance);
-}
-);
+)
 fun createApp(): UTSJSONObject {
-    val app = createSSRApp(GenAppClass);
-    return UTSJSONObject(Map<String, Any?>(utsArrayOf(
-        utsArrayOf(
-            "app",
-            app
-        )
-    )));
+    val app = createSSRApp(GenAppClass)
+    return _uO("app" to app)
 }
 fun main(app: IApp) {
-    definePageRoutes();
-    defineAppConfig();
-    (createApp()["app"] as VueApp).mount(app, GenUniApp());
+    definePageRoutes()
+    defineAppConfig()
+    (createApp()["app"] as VueApp).mount(app, GenUniApp())
 }
 open class UniAppConfig : io.dcloud.uniapp.appframe.AppConfig {
-    override var name: String = "kux-pip";
-    override var appid: String = "__UNI__4CAF968";
-    override var versionName: String = "1.0.0";
-    override var versionCode: String = "100";
-    override var uniCompilerVersion: String = "4.36";
+    override var name: String = "shop-app"
+    override var appid: String = "__UNI__F95597E"
+    override var versionName: String = "1.0.0"
+    override var versionCode: String = "100"
+    override var uniCompilerVersion: String = "4.72"
     constructor() : super() {}
 }
 fun definePageRoutes() {
-    __uniRoutes.push(UniPageRoute(path = "pages/index/index", component = GenPagesIndexIndexClass, meta = UniPageMeta(isQuit = true), style = utsMapOf("navigationBarTitleText" to "uni-app x")));
-    __uniRoutes.push(UniPageRoute(path = "pages/index2/index2", component = GenPagesIndex2Index2Class, meta = UniPageMeta(isQuit = false), style = utsMapOf("navigationBarTitleText" to "", "enablePullDownRefresh" to false, "navigationStyle" to "custom")));
+    __uniRoutes.push(UniPageRoute(path = "pages/index/index", component = GenPagesIndexIndexClass, meta = UniPageMeta(isQuit = true), style = _uM("navigationBarTitleText" to "uni-app x")))
 }
-val __uniLaunchPage: Map<String, Any?> = utsMapOf("url" to "pages/index/index", "style" to utsMapOf("navigationBarTitleText" to "uni-app x"));
+val __uniLaunchPage: Map<String, Any?> = _uM("url" to "pages/index/index", "style" to _uM("navigationBarTitleText" to "uni-app x"))
 fun defineAppConfig() {
-    __uniConfig.entryPagePath = "/pages/index/index";
-    __uniConfig.globalStyle = utsMapOf("navigationBarTextStyle" to "black", "navigationBarTitleText" to "uni-app x", "navigationBarBackgroundColor" to "#F8F8F8", "backgroundColor" to "#F8F8F8");
+    __uniConfig.entryPagePath = "/pages/index/index"
+    __uniConfig.globalStyle = _uM("navigationBarTextStyle" to "black", "navigationBarTitleText" to "uni-app x", "navigationBarBackgroundColor" to "#F8F8F8", "backgroundColor" to "#F8F8F8")
     __uniConfig.getTabBarConfig = fun(): Map<String, Any>? {
-        return null;
+        return null
     }
-    ;
-    __uniConfig.tabBar = __uniConfig.getTabBarConfig();
-    __uniConfig.conditionUrl = "";
-    __uniConfig.uniIdRouter = utsMapOf();
-    __uniConfig.ready = true;
+    __uniConfig.tabBar = __uniConfig.getTabBarConfig()
+    __uniConfig.conditionUrl = ""
+    __uniConfig.uniIdRouter = _uM()
+    __uniConfig.ready = true
 }
 open class GenUniApp : UniAppImpl() {
     open val vm: GenApp?
         get() {
-            return getAppVm() as GenApp?;
+            return getAppVm() as GenApp?
         }
     open val `$vm`: GenApp?
         get() {
-            return getAppVm() as GenApp?;
+            return getAppVm() as GenApp?
         }
 }
 fun getApp(): GenUniApp {
-    return getUniApp() as GenUniApp;
+    return getUniApp() as GenUniApp
 }
--- uniappx/src/main/java/pages/index/index.kt
+++ uniappx/src/main/java/pages/index/index.kt
@@ -1,62 +1,50 @@
-@file:Suppress("UNCHECKED_CAST", "USELESS_CAST", "INAPPLICABLE_JVM_NAME", "UNUSED_ANONYMOUS_PARAMETER")
-package uni.UNI4CAF968;
-import io.dcloud.uniapp.*;
-import io.dcloud.uniapp.extapi.*;
-import io.dcloud.uniapp.framework.*;
-import io.dcloud.uniapp.runtime.*;
-import io.dcloud.uniapp.vue.*;
-import io.dcloud.uniapp.vue.shared.*;
-import io.dcloud.uts.*;
-import io.dcloud.uts.Map;
-import io.dcloud.uts.Set;
-import io.dcloud.uts.UTSAndroid;
-import kotlinx.coroutines.CoroutineScope;
-import kotlinx.coroutines.Deferred;
-import kotlinx.coroutines.Dispatchers;
-import kotlinx.coroutines.async;
-import io.dcloud.uniapp.extapi.navigateTo as uni_navigateTo;
+@file:Suppress("UNCHECKED_CAST", "USELESS_CAST", "INAPPLICABLE_JVM_NAME", "UNUSED_ANONYMOUS_PARAMETER", "NAME_SHADOWING", "UNNECESSARY_NOT_NULL_ASSERTION")
+package uni.UNIF95597E
+import io.dcloud.uniapp.*
+import io.dcloud.uniapp.extapi.*
+import io.dcloud.uniapp.framework.*
+import io.dcloud.uniapp.runtime.*
+import io.dcloud.uniapp.vue.*
+import io.dcloud.uniapp.vue.shared.*
+import io.dcloud.uts.*
+import io.dcloud.uts.Map
+import io.dcloud.uts.Set
+import io.dcloud.uts.UTSAndroid
 open class GenPagesIndexIndex : BasePage {
-    constructor(__ins: ComponentInternalInstance) : super(__ins) {}
+    constructor(__ins: ComponentInternalInstance, __renderer: String?) : super(__ins, __renderer) {
+        onLoad(fun(_: OnLoadOptions) {}, __ins)
+    }
+    @Suppress("UNUSED_PARAMETER", "UNUSED_VARIABLE")
+    override fun `$render`(): Any? {
+        val _ctx = this
+        val _cache = this.`$`.renderCache
+        return _cE("view", null, _uA(
+            _cE("image", _uM("class" to "logo", "src" to "/static/logo.png")),
+            _cE("text", _uM("class" to "title"), _tD(_ctx.title), 1)
+        ))
+    }
+    open var title: String by `$data`
+    @Suppress("USELESS_CAST")
+    override fun data(): Map<String, Any?> {
+        return _uM("title" to "Hello")
+    }
     companion object {
-        @Suppress("UNUSED_PARAMETER", "UNUSED_VARIABLE")
-        var setup: (__props: GenPagesIndexIndex) -> Any? = fun(__props): Any? {
-            val __ins = getCurrentInstance()!!;
-            val _ctx = __ins.proxy as GenPagesIndexIndex;
-            val _cache = __ins.renderCache;
-            val title = ref("Hello");
-            val goToPage2 = fun(){
-                uni_navigateTo(NavigateToOptions(url = "/pages/index2/index2"));
-            }
-            ;
-            return fun(): Any? {
-                return createElementVNode("view", utsMapOf("class" to "content"), utsArrayOf(
-                    createElementVNode("image", utsMapOf("class" to "logo", "src" to "/static/logo.png")),
-                    createElementVNode("view", utsMapOf("class" to "text-area"), utsArrayOf(
-                        createElementVNode("text", utsMapOf("class" to "title"), toDisplayString(unref(title)), 1)
-                    )),
-                    createElementVNode("button", utsMapOf("onClick" to goToPage2), "打开页面2")
-                ));
-            }
-            ;
+        val styles: Map<String, Map<String, Map<String, Any>>> by lazy {
+            _nCS(_uA(
+                styles0
+            ), _uA(
+                GenApp.styles
+            ))
         }
-        ;
-        val styles: Map<String, Map<String, Map<String, Any>>>
-            get() {
-                return normalizeCssStyles(utsArrayOf(
-                    styles0
-                ), utsArrayOf(
-                    GenApp.styles
-                ));
-            }
         val styles0: Map<String, Map<String, Map<String, Any>>>
             get() {
-                return utsMapOf("content" to padStyleMapOf(utsMapOf("display" to "flex", "alignItems" to "center", "justifyContent" to "center")), "logo" to padStyleMapOf(utsMapOf("height" to "200rpx", "width" to "200rpx", "marginTop" to "200rpx", "marginBottom" to "50rpx")), "title" to padStyleMapOf(utsMapOf("fontSize" to "36rpx", "color" to "#8f8f94")));
+                return _uM("logo" to _pS(_uM("height" to 100, "width" to 100, "marginTop" to 100, "marginRight" to "auto", "marginBottom" to 25, "marginLeft" to "auto")), "title" to _pS(_uM("fontSize" to 18, "color" to "#8f8f94", "textAlign" to "center")))
             }
-        var inheritAttrs = true;
-        var inject: Map<String, Map<String, Any?>> = utsMapOf();
-        var emits: Map<String, Any?> = utsMapOf();
-        var props = normalizePropsOptions(utsMapOf());
-        var propsNeedCastKeys: UTSArray<String> = utsArrayOf();
-        var components: Map<String, CreateVueComponent> = utsMapOf();
+        var inheritAttrs = true
+        var inject: Map<String, Map<String, Any?>> = _uM()
+        var emits: Map<String, Any?> = _uM()
+        var props = _nP(_uM())
+        var propsNeedCastKeys: UTSArray<String> = _uA()
+        var components: Map<String, CreateVueComponent> = _uM()
     }
 }
